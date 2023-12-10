3ad5ea5d629ac60d76025e6b61f895f7dc37d758
Bob Verity <bobverity1@gmail.com>
2023-12-10 07:55:17 -0500

    nice progress bars

--- R/utils.R
+++ R/utils.R
@@ -34,16 +34,7 @@ rcpp_to_matrix <- function(x) {
 
 #------------------------------------------------
 # update progress bar
-# pb_list = list of progress bar objects
-# name = name of this progress bar
-# i = new value of bar
-# max_i = max value of bar (close when reach this value)
-# close = whether to close when reach end
-#' @importFrom utils setTxtProgressBar
 #' @noRd
-update_progress <- function(pb_list, name, i, max_i, close = TRUE) {
-  setTxtProgressBar(pb_list[[name]], i)
-  if (i == max_i & close) {
-    close(pb_list[[name]])
-  }
+update_progress <- function(pb_list, name) {
+  knitrProgressBar::update_progress(pb_list$pb_env[[name]])
 }
